f4cd3789d3f0fb80b0bcd6a4ea68a7a1484c39d4
Shiboshree Roy <shiboshreeroy169@gmail.com>
2024-12-27 15:24:02 +0600

    Update Likes Controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,18 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @post = Post.find(params[:post_id])
+    unless current_user.likes.exists?(post: @post)
+      @post.likes.create(user: current_user)
+    end
+    redirect_to posts_path
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @like = current_user.likes.find_by(post: @post)
+    @like.destroy if @like
+    redirect_to posts_path
+  end
 end
