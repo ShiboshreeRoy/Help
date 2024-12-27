73bfc78a535df453993e6d5a1b5f418b07089620
Shiboshree Roy <shiboshreeroy169@gmail.com>
2024-12-27 15:22:16 +0600

    Update Comments Controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to posts_path, notice: 'Comment added successfully!'
+    else
+      redirect_to posts_path, alert: 'Comment could not be added.'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
